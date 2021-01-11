5cb57b62ed9d400ec46867c5248cf831fbea1033
DipeshLama <dipeslama32@gmail.com>
2021-01-11 14:48:35 +0545

    Login Activity

--- app/src/main/java/com/dipesh/softuserclone/LoginActivity.kt
+++ app/src/main/java/com/dipesh/softuserclone/LoginActivity.kt
@@ -6,14 +6,14 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
-class LoginActivity : AppCompatActivity(),View.OnClickListener {
-    private lateinit var etUsername:EditText
+class LoginActivity : AppCompatActivity(),View.OnClickListener{
+    private lateinit var etUsername: EditText
     private lateinit var etPassword:EditText
-    private lateinit var btnLogin:Button
-
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         etUsername=findViewById(R.id.etUserName)
         etPassword=findViewById(R.id.etPassword)
         btnLogin=findViewById(R.id.btnLogin)
@@ -22,12 +22,12 @@ class LoginActivity : AppCompatActivity(),View.OnClickListener {
     private fun isValid():Boolean{
         when{
             etUsername.text.isEmpty()==true->{
-                etUsername.error="Field must not be empty"
+                etUsername.error="Please enter username"
                 return false
             }
 
             etPassword.text.isEmpty()==true->{
-                etPassword.error="Field must not be empty"
+                etPassword.error="please enter password"
                 return false
             }
         }
@@ -35,10 +35,6 @@ class LoginActivity : AppCompatActivity(),View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.btnLogin->{
 
-            }
-        }
     }
 }
